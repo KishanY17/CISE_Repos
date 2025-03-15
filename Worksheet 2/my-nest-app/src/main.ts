ae69c5a2e7a0758df5bc57dfbedfb53f07cfe5f6
KishanY17 <105463218+KishanY17@users.noreply.github.com>
2025-03-15 17:08:44 +1300

    Created a dotenv and nodemon
    Dotenv was created to prevent the need to manually input port numbers and nodemon was created to allow the app to auto refresh

--- Worksheet 2/my-nest-app/src/main.ts
+++ Worksheet 2/my-nest-app/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3002);
+  dotenv.config();
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
+ 
 }
 bootstrap();
